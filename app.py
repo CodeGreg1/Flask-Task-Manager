8c9b5f1162f680be116ab4002e37d70175380e96
GregWebDev <code@greggoodrem.co.uk>
2022-09-17 13:39:40 +0000

    Update task into database

--- app.py
+++ app.py
@@ -123,14 +123,14 @@ def add_task():
 def edit_task(task_id):
     if request.method == "POST":
         is_urgent = "on" if request.form.get("is_urgent") else "off"
-        submit = {
+        submit = {"$set":{
             "category_name": request.form.get("category_name"),
             "task_name": request.form.get("task_name"),
             "task_description": request.form.get("task_description"),
             "is_urgent": is_urgent,
             "due_date": request.form.get("due_date"),
             "created_by": session["user"]
-        }
+        }}
         mongo.db.tasks.update_one({"_id": ObjectId(task_id)}, submit)
         flash("Task Successfully Updated")
 
